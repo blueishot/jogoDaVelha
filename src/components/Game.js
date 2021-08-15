7f16945f8f253548a80cc561e2af39bc47edc806
blueishot <fabricioteixeirahungria@gmail.com>
2021-08-15 20:26:56 -0300

    board Javascript

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,48 @@
 import { useState } from 'react';
 import gameArt from '../css/gameArt.css'
+import Square from '../components/Square'
 
 const Game = () => {
    
-    const [choice, setChoice] = useState('')
+    const [turn, setTurn] = useState('X')
+    const [piece, setPiece] = useState(Array(9).fill(''));
+    const [winner, setWinner] = useState ()
 
-    let currentMove = 'X'
+    const checkWinner = (squarePiece) => {
+        let combos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+    }
 
-    const moveTurn = () => {
-        if (currentMove == 'O') {
-            currentMove = 'X'
-        } else {
-            currentMove = 'O'
+    const changePiece = (sqr) => {
+        if (piece[sqr] !== '') {
+            alert("You can't do this")
+            return;
         }
+
+        let squarePiece = [...piece];
+
+        if (turn === 'X') {
+            squarePiece[sqr] = 'X';
+            setTurn('O');
+        } else {
+            squarePiece[sqr] = 'O';
+            setTurn('X');
+        }
+
+        checkWinner (squarePiece);
+        setPiece (squarePiece);
+    }
+
+    const Square = ({sqr}) => {
+        return <div className='boardGame' onClick={() => changePiece(sqr)}>{piece[sqr]}</div>
     }
    
     return (
@@ -50,37 +80,20 @@ const Game = () => {
             </div>
             <div className='ticTacToeBoard'>
                 <div>
-                    <div className='boardGame' onClick={(moveTurn) => setChoice (currentMove)}>
-                        {choice}    
-                    </div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
+                    <Square sqr={0}/>
+                    <Square sqr={1}/>
+                    <Square sqr={2}/>
+
                 </div>
                 <div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
+                    <Square sqr={3}/>
+                    <Square sqr={4}/>
+                    <Square sqr={5}/>
                 </div>
                 <div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
-                    <div className='boardGame' onClick={() => setChoice (currentMove)}>
-                        {choice}
-                    </div>
+                    <Square sqr={6}/>
+                    <Square sqr={7}/>
+                    <Square sqr={8}/>
                 </div>
             </div>
         </div>
---
+++ src/components/Square.js
@@ -0,0 +1,25 @@
+
+const Square = () => {
+    return (
+        <div 
+        style={{
+            backgroundColor:'#FFFFFF',
+            width:'160px',
+            height:'160px',
+            marginBottom:'15px',
+            marginLeft:'15px',
+            borderRadius:'15px',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+            color:'#828282',
+            fontSize:'130px',
+            fontWeight:'bold',
+            cursor:'pointer',
+        }}>
+            
+        </div>
+    )
+}
+
+export default Square;
